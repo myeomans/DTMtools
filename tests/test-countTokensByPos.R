025cd6c85fd9cb364fd2ff66532800bba5849a85
AlejandroKantor <akantorb@gmail.com>
2017-11-15 17:17:30 -0500

    Also call df_token_single_pos

--- tests/test-countTokensByPos.R
+++ tests/test-countTokensByPos.R
@@ -12,10 +12,11 @@ l_token_pos <- countTokensByPos(texts)
 
 df_token_pos_text <- l_token_pos$df_token_pos_text
 df_token_multiple_pos <- l_token_pos$df_token_multiple_pos
+df_token_multiple_pos <- l_token_pos$df_token_single_pos
 
 View(df_token_multiple_pos)
-
 lookUpTokenPos(df_token_pos_text, s_token_pos = "much_ADJ")
+
 
 lookUpTokenPos(df_token_pos_text, s_token_pos = "purchase_NOUN")
 
