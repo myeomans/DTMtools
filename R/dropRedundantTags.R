19dd50bce76d414992d42f7af73d14fd80bf6a77
myeomans <mk.yeomans@gmail.com>
2018-02-01 12:55:59 -0500

    pipe import

--- R/dropRedundantTags.R
+++ R/dropRedundantTags.R
@@ -1,5 +1,9 @@
-
+#' Tag Dropper
+#' @description Drop redundant parses
+#' @param texts a list of character vectors
+#' @return a list of character vectors
 #' @import tidyverse
+#' @import magrittr
 dropRedundantTags <- function(pos_lists, sparse=0.99){
   dt_pos <- tibble(raw = (unlist(pos_lists))) %>%
     mutate(token = gsub("_[A-Z]+$","",raw)) %>%
